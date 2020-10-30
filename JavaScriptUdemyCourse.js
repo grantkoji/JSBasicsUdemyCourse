73bfc2c0d2886aaa0e48f2f157c4a5686a5c84c1
grantkoji <grantkoji@gmail.com>
2020-10-30 10:59:45 -0400

    with numbers and syntax'

--- JavaScriptUdemyCourse.js
+++ JavaScriptUdemyCourse.js
@@ -1,4 +1,63 @@
 
 
+There is an organization called Ecma 
+International group in charge of all sorts of different standards 
+For programming but also for electromagnetic fields 
+But also for ECMA script 
+A programming language should ) 
+behave this way or that way
+its a desription of how the langauge should work 
 
+Different browsers will support different features at any given time 
+
+Compatability chart to tell me which browsers support arrow functions
+Not supported by ie 
+Added to Chrome and Firefox
+Not every feature is available to every browser 
+All these things 
+Javascript
+Ecmascript
+ES2018
+ES2017...ES2016
+ES5...ES6 
+
+mdn closest thing to official documentation of javascript that we have
+
+Primitive Types
+Number
+String
+Boolean
+Null
+Undefined
+
+Working in the console tab 
+Nothing you write is actually saved 
+Easy for us to explore things without getting caught up in work flow 
+Usually console is for debugging 
+Not for writing actual applications 
+
+Change colors to make it easier to see )
+Three dots to change where icon is located 
+
+5 ** 2 is same as 5 ^ 2 
+Exponents in javascript 
+
+Javascript does follow standard order of operations 
+Parenthesis, Exponent, Multiplication, Division, Addition, Subtraction
+PEMDAS 
+
+1 + NaN = NaN 
+1 / 0 = NaN 
+Needs to be NaN  
+Needs to be written out that way 
+
+Can write out Infinity or 1/0 
+Also -Infinity 
+
+0 and -0 are values in Javascript as well 
+
+Uncaught SyntaxError: Identify 'averageRating' has already been declared 
+If I try to call let twice 
+
+kdlafjl";
 
