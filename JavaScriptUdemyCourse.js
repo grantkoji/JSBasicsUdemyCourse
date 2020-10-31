1e90508e537f9dee7d5c01860929a589d47191d5
grantkoji <grantkoji@gmail.com>
2020-10-31 18:13:46 -0400

    more basicssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss

--- JavaScriptUdemyCourse.js
+++ JavaScriptUdemyCourse.js
@@ -59,5 +59,144 @@ Also -Infinity
 Uncaught SyntaxError: Identify 'averageRating' has already been declared 
 If I try to call let twice 
 
-kdlafjl";
+score = score + 10
+score += 10
+score -= 10
+score *= 10
+
+let bonusMult = 3
+score *= bonusMult 
+Is multiplying by 3
+
+Unary operators:
+counter++
+counter--
+
+
+Const error 
+Uncaught typeError 
+
+Uncaaught syntax error unexpected token for an open string without closing it
+
+)
+"she said "omg""
+//Uncaught SyntaxError: Unexpected Identifier 
+
+Can add strings 
+
+firstName + ' ' + lastName
+
+"hi" + 1 => "hi1"
+For netative
+"hi" - 2 => NaN 
+;
+
+Strings are indexed
+0 to string.length - 1 
+
+Every string has a property called length 
+"hello friend!".length 
+=> 13
+B/c it has 5 characters
+
+mySong[0] = "S"
+
+mySong[13] => undefined 
+If it's for a character whose index is not there '
+
+mySong[mySong.length -1]
+returns last character in string 
+
+STRINGS ARE IMMUTABLE IN AVASCRIPT
+let favSong = "Surfin' USA"
+IF I DO 
+favSong[0] ///=> "S"
+favSong[0] = "D"
+favSong ///=> "Surfin' USA"
+Does not change first to "D"
+Strings are immutable in JS 
+
+String methods all have .method() notation 
+Lots of methods to look up on a sztring 
+.toUpperCase()
+New version of the string completely upper case
+But not changing initila string 
+let msg= "you are so grounded mr"
+msg.toUpperCase() //=> "YOU ARE SO GROUNDED MR"
+but msg //=> "you are so grounded mr"
+MSG NOT CHANGING B/C STRINGS ARE IMMUTABLE 
+
+.toLowerCase()
+FORGET TO put in message and action then we will 
+
+greeting.trim() returns string without spaces in front and behind string 
+let color = "    purple robot   "
+color.trim() //=> "purple robot"
+color //=> "    purple robot    "
+color.trim().toUpperCase() //=> "PURPLE ROBOT"
+Thumbs down still work in MDN, but not spposed to use them anymore 
+
+let tvShow = 'catdog'
+tvShow.indexOf('cat') //=> 0
+tvShow.indexOf('dog') //=> 3
+tvShow.indexOf('z') //=> -1 (not found)
+ tvShow.slice(3) //=> 'dog'
+ tvShow //=> 'catdog' b/c strings are immutable 
+ 'superhero'.slice(0,5) => 'super'
+ The start is first argument, second argument is one index after end 
+
+ let annoyignLaugh = 'so funny! ha'
+ annoyignLaugh.replace('ha', 'haha') //=> 'so funny! haha'
+
+ 'ha ha ha'.replace('ha', 'hee') //=> 'hee ha ha'
+ Only replaces the first 'ha', not all of them
+
+ In MDN, optional parameters in brackets 
+ str.slice(beginIndex[, endIndex])
+ [] Tellsus it is optional
+
+'PUP'[3] //=> undefined 
+
+let park = 'Yellowstone'
+const index = park.indexOf('Stone')
+index //=> -1 bc 'Stone' is not in park
+
+let yell = 'GO AWAY!!'
+let index = yell.indexOf('!')
+index //=> 7 
+  B/c index of stops at first patch 
+
+'GARBAGE!'.slice(2).replace('B', '')
+//=> 'RAGE!'
+
+String escapes
+ Also known as escape sequences 
+ Combinations of characters with a string 
+ 
+ "He said I ain\'t happy"
+ //=> "He said I ain't happy"
+
+ "He said \"I ain\'t happy\""
+ //=> "He said "I ain't happy""
+
+//  \' or \" gives you the quote without being part of string 
+'Hello\nGoodbye'
+//=> 'Hello 
+// Goodbye'
+\n jumps a line in string 
+\t moves a tab over
+
+Look up MDN escape notation 
+
+If you need a back slash in a string \\
+"I am backslashed\\" => 
+"\" Is an error uncaught syntax error invalid
+'\\' => '\'
+"
+`${1+5}` => "6"
+
+can do `${variable.toUpperCase()}`
+
+`total price: $${price*quantity}`
+ string kdlafjl";
 
