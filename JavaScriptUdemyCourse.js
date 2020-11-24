04389e2653d177f702b2adde6ab66f25e39a8a5b
grantkoji <grantkoji@gmail.com>
2020-11-23 23:50:50 -0500

    with password problem with username and using true/false statements in return

--- JavaScriptUdemyCourse.js
+++ JavaScriptUdemyCourse.js
@@ -1380,6 +1380,47 @@ function containsPurple(arr){
   return false;
 }
 
+//password must be at least 8 characters
+//cannot contain spaces 
+//cannot contain the username
+function isValidPassword(password, username){
+  if(password.length < 8){
+    return false;
+  }
+  if(password.indexOf(' ') !== -1){
+    return false;
+  }
+  if(password.indexOf(username) !== -1){
+    return false;
+  }
+  return true;
+}
+
+function isValidPassword(password, username){
+  if(password.length < 8 
+    || password.indexOf(' ') !== -1
+    || password.indexOf(username) !== -1){
+    return false;
+  }
+  return true;
+}
+
+function isValidPassword(password, username){
+  const tooShort = password.length < 8;
+  const hasSpace = password.indexOf(' ') !== -1;
+  const tooSimilar = password.indexOf(username) !== -1;
+  
+  if(!tooShort && !hasSpace && !tooSimilar){
+    return true;
+  }
+  return false;
+}
 
 
+function isValidPassword(password, username){
+  const tooShort = password.length < 8;
+  const hasSpace = password.indexOf(' ') !== -1;
+  const tooSimilar = password.indexOf(username) !== -1;
+  return !tooShort && !hasSpace && !tooSimilar
+}
 
