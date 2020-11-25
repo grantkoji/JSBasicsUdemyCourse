5e3867764d4dc4d942b716e0dc67419d95535c70
grantkoji <grantkoji@gmail.com>
2020-11-24 22:36:49 -0500

    with adding values to cards

--- JavaScriptUdemyCourse.js
+++ JavaScriptUdemyCourse.js
@@ -1462,3 +1462,34 @@ function isPangram(sentence){
  }
  return true;
 }
+
+function pick(arr){
+  //return random element from array
+  const idx = Math.floor(Math.random() * arr.length)
+  return arr[idx]
+}
+
+function getCard(){
+  const values = [
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    '10',
+    'J',
+    'Q',
+    'K',
+    'A'
+  ]
+  // const value = pick(values);
+  const suits = ['clubs', 'spades', 'hearts', 'diamonds']
+  return { value: pick(values), suit: pick(suits)}
+}
+
+
+
